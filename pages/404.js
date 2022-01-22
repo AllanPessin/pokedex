1715021c59a7d2e3352d0340048a5e852472065b
Allan Da Silva Pessin <allan.pessin@outlook.com>
2022-01-22 19:17:14 -0300

    page 4040 created

--- pages/404.js
+++ pages/404.js
@@ -1,13 +1,14 @@
+import styles from "../styles/404.module.css";
 import Link from "next/link"
 
 export default function NotFound() {
   return (
-    <>
-    <h1>404</h1>
-    <p>Parece que este pokemon não foi capturado</p>
-    <Link href={"/"}>
-      <a>Voltar</a>
-    </Link>
-    </>
+    <div className={styles.not_found}>
+      <h1>404</h1>
+      <p>Parece que este pokemon não foi capturado</p>
+      <Link href={"/"}>
+        <a>Voltar</a>
+      </Link>
+    </div>
   )
 }
